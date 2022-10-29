4bfeadd078b917694d6b3972d0d5c5f0883a25bc
Supakorn Netsuwan <72784696+SupakornNetsuwan@users.noreply.github.com>
2022-10-29 17:26:30 +0700

    Update : add javascript for button tag

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello TOBEIT!")
+let counter = 0; // เราสร้างตัวแปรด้วย let ชื่อว่า counter และกำหนดค่าเริ่มต้นเป็น 0
+
+const visitFigma = () => {
+    counter++; // ทุกครั้งที่ Function นี้ถูกเรียกใช้ ค่าของ counter จะเพิ่มขึ้น 1
+    const topic = document.getElementById("blog-topic");
+    const content = document.getElementById("blog-content");
+
+    topic.innerHTML = "This is figma! 📝";
+    content.innerHTML = "Today we're going to use figma for designing your first webapge. <br/>clicked time is " + counter;
+
+    topic.style.color = "black"
+}
